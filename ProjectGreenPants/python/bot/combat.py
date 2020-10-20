3f5875185c6026845e381244a0cd3238d5a7b9c8
cread mefford <creadmefford@hotmail.com>
2020-10-20 16:44:33 -0700

    add a combat script for porting the c++ code from gpr.

---
+++ ProjectGreenPants/python/bot/combat.py
@@ -0,0 +1,9 @@
+def attack (skill, username):
+    result = f"[ATT] @{username} [SL]:+5  [Roll]:18 [Bi/Quad]: L-Leg/BL-Leg"
+    return result
+
+def defend (skill, username):
+    result = f"[DEF] @{username} [SL]:+5  [Roll]:18 [Bi/Quad]: L-Leg/BL-Leg"
+    return result
+
+    
--- ProjectGreenPants/python/bot/gprbot.py
+++ ProjectGreenPants/python/bot/gprbot.py
@@ -3,20 +3,13 @@
 # This program is dedicated to the public domain under the CC0 license.
 
 """
-Simple Bot to reply to Telegram messages.
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
+green pants revenge bot
 """
 
 import logging
 import os
 import random
-
+from combat import attack, defend
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
@@ -24,7 +17,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -38,12 +30,22 @@ def help(update, context):
 
 def attack_handler(update, context):
     username = update.message.from_user.first_name
-    response = f"[ATT] @{username} [SL]:+5  [Roll]:18  [Bi/Quad]: L-Leg/BL-Leg"   
+    #response = f"[ATT] @{username} [SL]:+5  [Roll]:18  [Bi/Quad]: L-Leg/BL-Leg"   
+    response = ""
+    if len (context.args) == 0 or not context.args[0].isdigit():
+        response = "attack usage: /a <skill>\r\n\t\twhere <skill> is your weapon skill + any combat modifiers"
+    else:
+        response = attack (int(context.args[0]), username)
     context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 def defend_handler(update, context):
     username = update.message.from_user.first_name
-    response = f"[DEF] @{username} [SL]:+5  [Roll]:18  [Bi/Quad]: L-Leg/BL-Leg"   
+    #response = f"[DEF] @{username} [SL]:+5  [Roll]:18  [Bi/Quad]: L-Leg/BL-Leg"   
+    response = ""
+    if len (context.args) == 0 or not context.args[0].isdigit():
+        response = "defend usage: /d <skill>\r\n\t\twhere <skill> is your weapon skill + any combat modifiers"
+    else:
+        response = defend (int(context.args[0]), username)
     context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 def roll_handler(update, context):
@@ -56,11 +58,9 @@ def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
-
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
-
 
 def main():
     """Start the bot."""
