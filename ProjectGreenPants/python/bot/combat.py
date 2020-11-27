be6ac52f1221a20593de2d29ca8db3814b58cdd1
cread mefford <creadmefford@hotmail.com>
2020-11-27 08:43:57 -0800

    - support missfires on even numbered fumbles with explosives, blackpowder, engineering devices. - return -1 SL if the roll is higher than the skill level. - ensure a roll of 100 is also an auto-fumble (just like #99)

--- ProjectGreenPants/python/bot/combat.py
+++ ProjectGreenPants/python/bot/combat.py
@@ -16,10 +16,11 @@ def combatRoll(skill,username, wasAttack, roll):
     wasCrit = isCrit(roll, skill)
     wasImpaled = isImpaled(roll, skill)
     wasFumble = isFumble(roll, skill)
+    wasMissfire = isMissfire(roll, skill)
     wasAutoFail = isAutoFail (roll)
     wasAutoSuccess = isAutoSuccess (roll)
 
-    response = createResponse(wasAttack, wasFumble, wasCrit, wasImpaled, wasAutoFail, wasAutoSuccess, sl, roll, location, username)
+    response = createResponse(wasAttack, wasFumble, wasCrit, wasImpaled, wasMissfire, wasAutoFail, wasAutoSuccess, sl, roll, location, username)
     #print(response)
     return response 
 
@@ -32,7 +33,7 @@ def flipDigits(flippee):
     ones = flippee % 10
     tens = flippee / 10
     result = (ones * 10) + tens
-        
+
     return int (result)
 
 def isCrit(roll, skill):
@@ -54,8 +55,15 @@ def isImpaled(roll, skill):
 def isFumble(roll, skill):
     if (roll % 11 == 0 and skill < roll):
         return True
-    elif roll == 99:
-        return True # 99 is auto fumble
+    elif roll == 99 or roll == 100:
+        return True # 99/100 is auto fumble
+    else:
+        return False
+
+# misfire is an even numbered fumble
+def isMissfire(roll, skill):
+    if isFumble (roll, skill) and roll % 2 == 0:
+        return True
     else: 
         return False
 
@@ -66,26 +74,28 @@ def isAutoSuccess(roll):
     return roll <= 5
 
 def successLevel(skill, roll):
-    tens = roll / 10
-    skill = skill / 10
+    rollTens = roll / 10
+    skillTens = skill / 10
 
-    success = int(skill) - int(tens)
+    success = int(skillTens) - int(rollTens)
+    if success == 0 and roll > skill:
+        success = -1
 
     if isAutoFail(roll):
         success = min(-1, success)
-    
+
     if isAutoSuccess(roll):
         success = max(1, success)
-    
+
     return int(success)
 
 def targetLocation(location):
     if location < 10:
         return "Head"
-    
+
     elif location < 25:
         return "Off Arm"
-    
+
     elif location < 45:
         return "Main Arm"
 
@@ -94,7 +104,7 @@ def targetLocation(location):
 
     elif location < 90:
         return "L-Leg"
-    
+
     elif location <= 100:
         return "R-Leg"
 
@@ -123,13 +133,13 @@ def beastLocation(location):
     else:
         return "ERROR"
 
-def createResponse(wasAttack, wasFumble, wasCrit, wasImpaled, wasAutoFail, wasAutoSuccess, SL, roll, location, username):
+def createResponse(wasAttack, wasFumble, wasCrit, wasImpaled, wasMissfire, wasAutoFail, wasAutoSuccess, SL, roll, location, username):
 
     if wasAttack:
         result = f"[ATT @{username}] " 
     else:
         result = f"[DEF @{username}] "
-    
+
     if SL > 0:
         result += "[SL:+" + str(int(SL)) + "] "
     else:
@@ -143,17 +153,19 @@ def createResponse(wasAttack, wasFumble, wasCrit, wasImpaled, wasAutoFail, wasAu
 
     if wasCrit:
         result += "{CRIT!} "
-    
+
     if wasImpaled:
         result += "{IMPALE!} "
 
-    if wasFumble:
+    if wasFumble and wasMissfire:
+        result += "{MISSFIRE/FUMBLE!} "
+    elif wasFumble:
         result += "{FUMBLE!} "
     elif not wasFumble:
         target = targetLocation(location)
         beast = beastLocation(location)
         result += "[Bi:" + target + "|Quad:" + beast + "]"
-    
+
     result += " [Roll:" + str(roll) +"] "
     return result
 
--- ProjectGreenPants/python/bot/test.py
+++ ProjectGreenPants/python/bot/test.py
@@ -1,6 +1,12 @@
-from combat import flipDigits, isCrit, isImpaled, isFumble, successLevel, targetLocation, beastLocation, attack, createResponse, defend, combatRoll
+from combat import flipDigits, isCrit, isImpaled, isFumble, isMissfire, successLevel, targetLocation, beastLocation, attack, createResponse, defend, combatRoll
 import random
 
+# "expected test"
+def ext(outcome, description):
+    if outcome == False:
+        print ("test failure: ")
+        print (description)
+        quit()
 
 result = combatRoll(47, "John", True, 65)
 print(result)
@@ -21,3 +27,28 @@ print (combatRoll(125, "cread", True, 3)) # when skill > 100
 # success
 print ("regular success:")
 print (combatRoll (120, "cread", True, 94))
+
+# missfire
+print ("missfires:")
+print (combatRoll (60, "John", True, 66))
+print (combatRoll (60, "John", True, 88))
+print (combatRoll (60, "John", True, 100))
+print (combatRoll (66, "John", True, 66)) # non-missfire
+print (combatRoll (66, "John", True, 77)) # non-missfire, fumble
+
+print ("\nstarting unit tests...")
+
+ext (isFumble (100, 50), "100 is auto fumble!")
+ext (isFumble (99, 50), "99 is auto fumble!")
+ext (not isFumble (98, 50), "98 is not a fumble!")
+ext (not isFumble (97, 50), "97 is not a fumble!")
+ext (not isFumble (96, 50), "96 is not a fumble!")
+
+ext (isMissfire (100, 50), "100 is an auto missfire!")
+ext (isMissfire (88, 50), "88 is missfire!")
+ext (isMissfire (66, 50), "66 is a missfire!")
+ext (not isMissfire (99, 50), "99 is a fumble but not a missfire!")
+ext (not isMissfire (77, 50), "77 is a fumble but not a missfire!")
+ext (not isMissfire (44, 50), "44 is NOT a missfire!")
+
+print ("all tests passed!\n")
